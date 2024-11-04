75a9bf7a7d5627c1cc1073735d4c95c634650b04
Serhat GENÇ <gncsrht05@gmail.com>
2024-11-04 08:58:56 +0300

    task1-2-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,11 @@
 function makeTransaction(quantity, pricePerDroid) {
-    
-    let toplam=quantity * pricePerDroid;
+  let toplam = quantity * pricePerDroid;
 
-    let mesaj=`You ordered   ${quantity}   droids worth   ${toplam}   droids worth `;
+  let mesaj = `You ordered   ${quantity}   droids worth   ${toplam}   droids worth `;
 
-    return mesaj;
-   
-  }
+  return mesaj;
+}
 
-  console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-  console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-  console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const content1=parseFloat(content);
+  const content1 = parseFloat(content);
   const padding1 = parseFloat(padding);
   const border1 = parseFloat(border);
 
-  const toplam= content1 + (padding1*2) + (border1*2);
+  const toplam = content1 + padding1 * 2 + border1 * 2;
   return toplam;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
